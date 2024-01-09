97370c2eed65054ecd2047a2c2fbe91fdf9fc642
Thom <22082476@student.hhs.nl>
2024-01-09 20:06:55 +0100

    fix deploy

--- React/my-react-app/src/Header-nav.js
+++ React/my-react-app/src/Header-nav.js
@@ -9,10 +9,10 @@ export function HeaderNav()
             <img src={img} alt='Logo stichting accesibility'></img>
             <h1 className='Title'>Any-Portal</h1>
             <div className='nav'>
-                <a href="/"><h2>Thuispagina</h2></a>
-                <a href="Onderzoek"><h2>Onderzoek</h2></a>
-                <a href="Account"><h2>Account</h2></a>
-                <a href="Logout"><h2>Logout</h2></a>
+                <a href="#/"><h2>Thuispagina</h2></a>
+                <a href="#/Onderzoek"><h2>Onderzoek</h2></a>
+                <a href="#/Account"><h2>Account</h2></a>
+                <a href="#/Logout"><h2>Logout</h2></a>
             </div>
         </div>
     );
